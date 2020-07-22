1bc8e5a822af9443d7fe8095165c16bd987e22af
jujeonghwan <jujeonghwan@gmail.com>
2020-07-22 16:53:13 -0400

    seconde commit (2020-07-22)

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_ohc=jsHCKeRk3K4AX_jqNW-&oh=a1d427daf07686700e5e940bc7686ff9&oe=5F42D9E7" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="#">Add New Post</a>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        @foreach($user->posts as post)
+            <div class="col-4">
+                <img src="/storage/{{ $post->image }}" class="w-100">
+            </div>
+        @endforeach
+    </div>
+
+</div>
+@endsection
